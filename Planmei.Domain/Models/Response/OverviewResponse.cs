c175a4af0b7b3ed0115a3db5cebb6916809b32c1
Joao Martins <vitorcastelo.m@gmail.com>
2025-09-08 12:16:15 -0300

    Create Overview response

---
+++ Planmei.Domain/Models/Response/OverviewResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Planmei.Domain.Models.Response
+{
+    public class OverviewResponse
+    {
+        public decimal Revenue { get; set; }
+        public decimal Expenses { get; set; }
+        public decimal TargetAmount { get; set; }
+
+    }
+}
