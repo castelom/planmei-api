0a82bfa77c55145893a0868a06fd6b8e40704a24
Joao Martins <vitorcastelo.m@gmail.com>
2025-09-08 06:10:08 -0300

    Create UserContext

---
+++ Planmei.Domain/Interfaces/Services/ICurrentUserService.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace Planmei.Domain.Interfaces.Services
+{
+    public interface ICurrentUserService
+    {
+        string UserId { get; }
+        string UserName { get; }
+        IEnumerable<Claim> Claims { get; }
+    }
+}
